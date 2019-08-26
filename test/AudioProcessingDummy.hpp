efb9dc4dd483e9a4e0b401ad2a50c4fe89e11647
Iñaki Baz Castillo <ibc@aliax.net>
2019-08-26 21:25:49 +0200

    Improve test/AudioProcessingDummy.hpp (still tests chrases)

--- test/AudioProcessingDummy.hpp
+++ test/AudioProcessingDummy.hpp
@@ -5,13 +5,26 @@
 #include "modules/audio_processing/gain_control_impl.h"
 #include "modules/audio_processing/level_estimator_impl.h"
 #include "modules/audio_processing/noise_suppression_impl.h"
+#include "modules/audio_processing/noise_suppression_proxy.h"
 #include "modules/audio_processing/voice_detection_impl.h"
 
 class AudioProcessingDummy : public webrtc::AudioProcessing
 {
 public:
-	int Initialize() override { return 0; }
-	int Initialize(const webrtc::ProcessingConfig& processing_config) override { return 0; }
+	struct ApmPublicSubmodules
+	{
+		ApmPublicSubmodules() {}
+
+		std::unique_ptr<webrtc::GainControlImpl> gain_control;
+		std::unique_ptr<webrtc::LevelEstimatorImpl> level_estimator;
+		std::unique_ptr<webrtc::NoiseSuppressionImpl> noise_suppression;
+		std::unique_ptr<webrtc::NoiseSuppressionProxy> noise_suppression_proxy;
+		std::unique_ptr<webrtc::VoiceDetectionImpl> voice_detection;
+	};
+
+public:
+	int Initialize() override { return 1; }
+	int Initialize(const webrtc::ProcessingConfig& processing_config) override { return 1; }
 	int Initialize(
 	  int capture_input_sample_rate_hz,
 	  int capture_output_sample_rate_hz,
@@ -20,7 +33,7 @@ public:
 	  ChannelLayout capture_output_layout,
 	  ChannelLayout render_input_layout) override
 	{
-		return 0;
+		return 1;
 	}
 	void ApplyConfig(const Config& config) override {}
 	void SetExtraOptions(const webrtc::Config& config) override {}
@@ -32,7 +45,7 @@ public:
 	size_t num_reverse_channels() const override { return 1; }
 	void set_output_will_be_muted(bool muted) override {}
 	void SetRuntimeSetting(RuntimeSetting setting) override {}
-	int ProcessStream(webrtc::AudioFrame* frame) override { return 0; }
+	int ProcessStream(webrtc::AudioFrame* frame) override { return 1; }
 	int ProcessStream(
 	  const float* const* src,
 	  size_t samples_per_channel,
@@ -42,7 +55,7 @@ public:
 	  ChannelLayout output_layout,
 	  float* const* dest) override
 	{
-		return 0;
+		return 1;
 	}
 	int ProcessStream(
 	  const float* const* src,
@@ -50,7 +63,7 @@ public:
 	  const webrtc::StreamConfig& output_config,
 	  float* const* dest) override
 	{
-		return 0;
+		return 1;
 	}
 	int ProcessReverseStream(webrtc::AudioFrame* frame) override { return 0; }
 	int AnalyzeReverseStream(
@@ -59,7 +72,7 @@ public:
 	  int sample_rate_hz,
 	  ChannelLayout layout) override
 	{
-		return 0;
+		return 1;
 	}
 	int ProcessReverseStream(
 	  const float* const* src,
@@ -67,14 +80,14 @@ public:
 	  const webrtc::StreamConfig& output_config,
 	  float* const* dest) override
 	{
-		return 0;
+		return 1;
 	}
-	int set_stream_delay_ms(int delay) override { return 0; }
-	int stream_delay_ms() const override { return 0; }
+	int set_stream_delay_ms(int delay) override { return 10; }
+	int stream_delay_ms() const override { return 10; }
 	bool was_stream_delay_set() const override { return false; }
 	void set_stream_key_pressed(bool key_pressed) override {}
 	void set_delay_offset_ms(int offset) override {}
-	int delay_offset_ms() const override { return 0; }
+	int delay_offset_ms() const override { return 10; }
 	void AttachAecDump(std::unique_ptr<webrtc::AecDump> aec_dump) override {}
 	void DetachAecDump() override {}
 	void AttachPlayoutAudioGenerator(
@@ -83,40 +96,33 @@ public:
 	void UpdateHistogramsOnCallEnd() override {}
 	webrtc::AudioProcessingStats GetStatistics(bool has_remote_tracks) const override
 	{
-		webrtc::AudioProcessingStats stats;
-
-		return stats;
+		return this->stats;
 	}
 	webrtc::GainControl* gain_control() const override
 	{
-		webrtc::GainControlImpl* gain_control;
-
-		return gain_control;
+		return this->public_submodules->gain_control.get();
 	}
 	webrtc::LevelEstimator* level_estimator() const override
 	{
-		webrtc::LevelEstimatorImpl* level_estimator;
-
-		return level_estimator;
+		return this->public_submodules->level_estimator.get();
 	}
 	webrtc::NoiseSuppression* noise_suppression() const override
 	{
-		webrtc::NoiseSuppressionImpl* noise_suppression;
-
-		return noise_suppression;
+		return this->public_submodules->noise_suppression_proxy.get();
 	}
 	webrtc::VoiceDetection* voice_detection() const override
 	{
-		webrtc::VoiceDetectionImpl* voice_detection;
-
-		return voice_detection;
+		return this->public_submodules->voice_detection.get();
 	}
 	webrtc::AudioProcessing::Config GetConfig() const override
 	{
-		AudioProcessing::Config config;
-
-		return config;
+		return this->config;
 	}
+
+private:
+	std::unique_ptr<ApmPublicSubmodules> public_submodules;
+	webrtc::AudioProcessingStats stats;
+	webrtc::AudioProcessing::Config config;
 };
 
 #endif // MSC_TEST_AUDIO_PROCESSING_DUMMY_HPP
